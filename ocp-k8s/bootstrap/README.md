77fadd262021374181b0d2cceb7ef8a03cbc371c
Kyle Walker <unixislife@gmail.com>
2025-12-05 09:39:52 -0600

    Cleanup of ArgoCD repo and new revision of bootstrapping

---
+++ ocp-k8s/bootstrap/README.md
@@ -0,0 +1,5 @@
+# Bootstrapping OCP
+This is just a basic setup for a new OpenShift cluster for development. It will set up and install the following Operators and get them ready for use:
+
+- OpenShift GitOps
+- OpenShift Pipelines
---
+++ ocp-k8s/bootstrap/bootstrap.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+## OpenShift GitOps
+setup_gitops(){
+oc get namespace openshift-gitops-operator &> /dev/null
+if [ $? -eq 0 ];then
+  echo "Namespace exists... Skipping"
+else
+  oc create namespace openshift-gitops-operator
+  oc create namespace openshift-gitops
+  oc create -f op_gitops.yaml
+  ## Generate GitOps Repo Secret
+  sed -e 's/SSH_KEY/'$(echo -n `/usr/bin/cat ~/.ssh/id_rsa` | base64 -w0)'/g' secret_gitops.yaml | oc create -n openshift-gitops -f -
+fi
+
+}
+
+## OpenShift Pipelines
+setup_pipelines(){
+oc get namespace openshift-pipelines &> /dev/null
+if [ $? -eq 0 ];then
+  echo "Namespace exists... Skipping"
+else
+  oc create namespace openshift-pipelines
+  oc create -f op_pipeline.yaml
+fi
+}
+
+setup_gitops
+setup_pipelines
